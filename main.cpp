98ec2bcab73de4fb62b3db080ddcf01757c736c4
Camelia Spinu <kspinudev@gmail.com>
2023-06-02 15:22:52 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
+void createDiscipline() {
+
+    cout << "We have created a new Discipline" << endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    createDiscipline();
+    createDiscipline();
+    createDiscipline();
     return 0;
 }
